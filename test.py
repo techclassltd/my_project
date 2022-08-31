37b4b0156074e2f5cd030a2b2fb70944d6afb9f3
Elham Hoseini <hoseini-e@shirazu.ac.ir>
2022-08-31 18:14:37 +0430

    Add a feature to print the email addresses in a text file

--- test.py
+++ test.py
@@ -1,3 +1,15 @@
 print('Hello, world!')
 a=1
 print('a= ',a)
+
+
+def my_function(fname):
+	file = open('email_address.txt', 'r')
+	Lines = file.readlines()
+  
+	count = 0
+	for line in Lines:
+    count += 1
+    print("Line{}: {}".format(count, line))
+	
+	
