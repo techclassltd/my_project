32aca8414548de80818fc0311c7f5610b65a6afc
Elham Hoseini <hoseini-e@shirazu.ac.ir>
2022-09-07 11:50:56 +0430

    Revert "Revert "Revert "Insert a comment line as a test"""
    This reverts commit 451ef3d3782ae30b1c05a1676eb5a833d8f322ac.

--- test.py
+++ test.py
@@ -1,10 +1,13 @@
+<<<<<<< HEAD
 # This is a test file
+=======
 print('A test line')
+>>>>>>> ee6dec8dc7b47c86fa9f8cd9ca387e7127387647
 print('Hello, world!')
 a=1
 print('a= ',a)
 
-# A fution to print emails
+
 def my_function(fname):
 	file = open('email_address.txt', 'r')
 	Lines = file.readlines()
